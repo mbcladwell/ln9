a3c4bd26e344e946309a9868cf7d538c97b8bb10
mbcladwell <mbcladwell@stihie.net>
2021-06-13 07:50:39 -0400

    3

---
+++ install-limsn.sh
@@ -0,0 +1,94 @@
+#!/bin/sh
+
+# We require Bash but for portability we'd rather not use /bin/bash or
+# /usr/bin/env in the shebang, hence this hack.
+if [ "x$BASH_VERSION" = "x" ]
+then
+    exec bash "$0" "$@"
+fi
+
+set -e
+
+[ "$UID" -eq 0 ] || { echo "This script must be run as root."; exit 1; }
+
+
+PAS=$'[ \033[32;1mPASS\033[0m ] '
+ERR=$'[ \033[31;1mFAIL\033[0m ] '
+WAR=$'[ \033[33;1mWARN\033[0m ] '
+INF="[ INFO ] "
+# ------------------------------------------------------------------------------
+#+UTILITIES
+
+_err()
+{ # All errors go to stderr.
+    printf "[%s]: %s\n" "$(date +%s.%3N)" "$1"
+}
+
+_msg()
+{ # Default message to stdout.
+    printf "[%s]: %s\n" "$(date +%s.%3N)" "$1"
+}
+
+_debug()
+{
+    if [ "${DEBUG}" = '1' ]; then
+        printf "[%s]: %s\n" "$(date +%s.%3N)" "$1"
+    fi
+}
+
+# Return true if user answered yes, false otherwise.
+# $1: The prompt question.
+prompt_yes_no() {
+    while true; do
+        read -rp "$1" yn
+        case $yn in
+            [Yy]*) return 0;;
+            [Nn]*) return 1;;
+            *) _msg "Please answer yes or no."
+        esac
+    done
+}
+
+welcome()
+{
+    cat<<"EOF"
+
+ _______________________  |  _ |_  _  _ _ _|_ _  _         
+|O O O O O O O O O O O O| |_(_||_)(_)| (_| | (_)| \/       
+|O O O O O O 1 O O O O O|                         /        
+|O O O O O O O O O O O O|  /\    _|_ _  _ _  _ _|_. _  _   
+|O O O O O O O O O O O O| /~~\|_| | (_)| | |(_| | |(_)| |  
+|O O 1 O O O O O 1 O 1 O|  _                               
+|O O O O O O O O O O O O| (  _ |   _|_. _  _  _            
+|O O O 1 O O O O O O O O| _)(_)||_| | |(_)| |_)    
+|O O O O O O O O O O O O|
+ -----------------------  info@labsolns.com
+
+This script installs LIMS*Nucleus on your system
+
+htt://www.labsolns.com
+
+EOF
+    echo -n "Press return to continue..."
+    read -r
+}
+
+
+main()
+{
+    local tmp_path
+    welcome
+
+    _msg "Starting installation ($(date))"
+
+
+    _msg "${INF}cleaning up ${tmp_path}"
+    rm -r "${tmp_path}"
+
+    _msg "${PAS}Guix has successfully been installed!"
+
+    # Required to source /etc/profile in desktop environments.
+    _msg "${INF}Please log out and back in to complete the installation."
+ }
+
+main "$@"
